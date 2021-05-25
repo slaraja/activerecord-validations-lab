a1e0dfeca7e324d371900194f33b5c592a7fda74
Stephanie La Raja <stephanielaraja@gmail.com>
2021-05-25 08:20:22 -0400

    commit tree 1fe947de04e896b4a6eeb2a5ef0ab732b94044ce

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w[Fiction NonFiction] }
+    
+    validate :is_clickbait
+
+    def is_clickbait
+        if title != nil && !title.include?("Won't Believe" || "Secret" || "Top[number]" || "Guess")
+            errors.add(:title, "Not clickbait-y")
+        end
+    end
+
 end
